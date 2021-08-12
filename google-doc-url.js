d1987bdf435b03723c54012c17aedd894975be36
sashadb <sashadbrown@gmail.com>
2021-08-12 12:30:49 -0400

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1Fr12V2b6t_4g3UMLeEZ3UyDtHNp09fzHNCXERAlU-3Y/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1Fr12V2b6t_4g3UMLeEZ3UyDtHNp09fzHNCXERAlU-3Y/edit?usp=sharing';
